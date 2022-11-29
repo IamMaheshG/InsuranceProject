d17296aa46e7a602825dfe002479cf08276d6bbb
Mahesh Gulivindala <gulivindala.mahesh@specsavers.com>
2022-11-30 00:24:12 +0530

    Adding Users

--- src/components/Profile/ProfileForm.js
+++ src/components/Profile/ProfileForm.js
@@ -1,18 +1,31 @@
-// import classes from './ProfileForm.module.css';
-
+import * as React from "react";
+import classes from './ProfileForm.module.css';
 const ProfileForm = () => {
+  const [users, setUsers] = React.useState([]);
+  const f = async () => {
+    const res = await fetch("https://reqres.in/api/users/");
+    const json = await res.json();
+    setUsers(json.data);
+  };
+  React.useEffect(() => {
+    f();
+  }, []);
   return (
-    // <form className={classes.form}>
-    //   <div className={classes.control}>
-    //     <label htmlFor='new-password'>New Password</label>
-    //     <input type='password' id='new-password' />
-    //   </div>
-    //   <div className={classes.action}>
-    //     <button>Change Password</button>
-    //   </div>
-    // </form>
-    <div>
-      You can selelect your planning here
+    <div className="App">
+      <div className={classes.flex}>
+        {users.length &&
+          users.map((user) => {
+            return (
+              <div key={user.id}>
+                <p>
+                  <strong>{user.first_name}</strong>
+                </p>
+                <p>{user.email}</p>
+                <img key={user.avatar} src={user.avatar} />
+              </div>
+            );
+          })}
+      </div>
     </div>
   );
 }
